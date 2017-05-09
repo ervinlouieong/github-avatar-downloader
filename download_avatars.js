15fef9e48914a4fa2a3f1210b74e689a7df86c98
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-09 00:48:51 +0000

    Create the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -2,22 +2,25 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "ervinlouieong";
+var GITHUB_TOKEN = "4998418068c8921ce5c044098f3a7e8c4cb0fbbd";
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  function callback (err, results) {
-    if (err) {
-      console.log("ERROR!!!", err);
-      return;
-    }
-    for (var i in results) {
-      var parsedResults = JSON.parse(results[i]);
-      repoOwner = parsedResults.login;
-      repoName = parsedResults.id
-    }
-    console.log(repoOwner,repoName);
-  };
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 
 };
+
+function callback (err, results) {
+  if (err) {
+    console.log("ERROR!!!", err);
+    return;
+  }
+  console.log(results);
+};
+
+getRepoContributors("jquery","jquery",callback);
 
 
 
