cf41ba4da39fcb7ebe8ca5612027709940c44ff6
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-09 03:45:08 +0000

    Make them required arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,52 +1,52 @@
 var request = require('request');
 var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+if (!(process.argv[2] && process.argv[3])) {
+  console.log("ERROR! You must specify both arguments.");
+} else {
 
-var GITHUB_USER = "ervinlouieong";
-var GITHUB_TOKEN = "4998418068c8921ce5c044098f3a7e8c4cb0fbbd";
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+  var GITHUB_USER = "ervinlouieong";
+  var GITHUB_TOKEN = "4998418068c8921ce5c044098f3a7e8c4cb0fbbd";
 
-  var requestOptions = {
-    uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {"User-Agent" : "GitHub Avatar Downloader - Student Project"}
+  function getRepoContributors(repoOwner, repoName, cb) {
+
+    var requestOptions = {
+      uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {"User-Agent" : "GitHub Avatar Downloader - Student Project"}
+    }
+
+    request.get(requestOptions, cb)
+
+  };
+
+  function callback (err, results, body) {
+    if (err) {
+      console.log("ERROR!!!", err);
+      return;
+    }
+    var parsedResults = JSON.parse(body);
+    for (var i in parsedResults){
+      var compiledAvatarUrl = parsedResults[i].avatar_url;
+      downloadImageByURL(compiledAvatarUrl,`avatars/${i}.jpg`);
+    }
+  };
+
+  getRepoContributors(process.argv[2],process.argv[3],callback);
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Response Status Code: ', response.statusCode);
+           console.log('Downloading image...');
+         })
+         .on('end', function() {
+            console.log('Downloading Complete.');
+         })
+         .pipe(fs.createWriteStream(filePath))
   }
-
-  request.get(requestOptions, cb)
-
-};
-
-function callback (err, results, body) {
-  if (err) {
-    console.log("ERROR!!!", err);
-    return;
-  }
-  var parsedResults = JSON.parse(body);
-  for (var i in parsedResults){
-    var compiledAvatarUrl = parsedResults[i].avatar_url;
-    downloadImageByURL(compiledAvatarUrl,`avatars/${i}.jpg`);
-  }
-};
-
-getRepoContributors(process.argv[2],process.argv[3],callback);
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Downloading image...');
-       })
-       .on('end', function() {
-          console.log('Downloading Complete.');
-       })
-       .pipe(fs.createWriteStream(filePath))
 }
-
-function print(avatar){
-  return avatar;
-};
-
