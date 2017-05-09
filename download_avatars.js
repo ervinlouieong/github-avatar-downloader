dc2a3cc7306b8aca9c86ce587b38f0913227fb3a
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-09 00:34:14 +0000

    Start the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  function callback (err, results) {
+    if (err) {
+      console.log("ERROR!!!", err);
+      return;
+    }
+    for (var i in results) {
+      var parsedResults = JSON.parse(results[i]);
+      repoOwner = parsedResults.login;
+      repoName = parsedResults.id
+    }
+    console.log(repoOwner,repoName);
+  };
+
+};
+
+
+
+
