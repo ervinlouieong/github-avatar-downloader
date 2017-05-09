ce391e6c6b828063cfcaa0df3841aeb372e05b39
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-09 02:43:54 +0000

    Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -7,22 +7,27 @@ var GITHUB_TOKEN = "4998418068c8921ce5c044098f3a7e8c4cb0fbbd";
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  //console.log(requestURL);
-  request.get(requestURL)
-          .on('data', function(data){
-            console.log(data);
-          })
+  var requestOptions = {
+    uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {"User-Agent" : "GitHub Avatar Downloader - Student Project"}
+  }
+
+  request.get(requestOptions, cb)
 
 };
 
-function callback (err, results) {
+function callback (err, results, body) {
   if (err) {
     console.log("ERROR!!!", err);
     return;
   }
-  console.log(results);
+  var parsedResults = JSON.parse(body);
+  for (var i in parsedResults){
+    console.log(parsedResults[i].avatar_url);
+  }
 };
+
+
 
 getRepoContributors("jquery","jquery",callback);
 
